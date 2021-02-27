b2f7a36cb40a879cb6a74896f6cea240be9aa6b3
Daniel Moura <daniel.dmo@live.com>
2021-02-27 00:57:49 -0300

    Conclusão do dia 3.

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,50 @@
+import styles from "../styles/components/ChallengeBox.module.css";
+import NextScript from "next/document";
+
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+
+export function ChallengeBox() {
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {activeChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe {activeChallenge.amount} xp</header>
+
+          <main>
+            <img src={`icons/${activeChallenge.type}.svg`} alt="Braço" />
+            <strong>Novo desafio</strong>
+            <p>{activeChallenge.description}</p>
+          </main>
+
+          <footer>
+            <button
+              type="button"
+              className={styles.challengeFailedButton}
+              onClick={resetChallenge}
+            >
+              Falhei
+            </button>
+
+            <button
+              type="button"
+              className={styles.challengeSucceededButton}
+            >
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um clico para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            Avance de level completando desafios.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import styles from '../styles/pages/Home.module.css'
 
+import Head from 'next/head';
+
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
-import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -24,6 +26,7 @@ export default function Home() {
         </div>
         
         <div>
+          <ChallengeBox />
         </div>
 
       </section>
